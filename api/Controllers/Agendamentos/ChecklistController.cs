468ac83c00eeeaba92826178a8374294c756880d
Lúcio Pimenta <luciosystem1@hotmail.com>
2021-02-19 19:13:56 -0300

    Implementado metodos PUT, DELETE e GET por ID na rota de Checklist

--- api/Controllers/Agendamentos/ChecklistController.cs
+++ api/Controllers/Agendamentos/ChecklistController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using api.Controllers.Login;
 using api.Dominio.Entidade.Agendamento;
@@ -19,14 +19,17 @@ namespace api.Controllers.Agendamentos
     [Route("api/[controller]")]
     public class ChecklistController : ControllerBase
     {
-
+        private readonly EntityService<Checklist> _entityService;
         private readonly ChecklistService _checklistService;         
         private readonly ILogger<LoginController> _logger;
 
         public ChecklistController(EntityContext context, ILogger<LoginController> logger)
         {
             _logger = logger;
-            _checklistService = new ChecklistService(new ChecklistRepositorio(context), new EntityRepositorio(context));;            
+            _checklistService = new ChecklistService(new ChecklistRepositorio(context), new EntityRepositorio(context));
+            _entityService = new EntityService<Checklist>(new EntityRepositorio(context));
+
+
         }
 
         [HttpPost]
@@ -45,6 +48,67 @@ namespace api.Controllers.Agendamentos
                 return StatusCode(401, new { er.Message });
             }
         }
-       
-    }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("{idCheckList}")]
+        public async Task<ActionResult> BuscarPorId(int idCheckList)
+        {
+          try
+          {
+            var checkList = await _entityService.BuscarPorId(checkList => checkList.Id == idCheckList);
+            return StatusCode(200, checkList);
+          }
+          catch (System.Exception er)
+          {
+            return StatusCode(401, new { er.Message });
+          }
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Put(int id, [FromBody] ChecklistVM checklist)
+        {
+          try
+          {
+            var Builder = BuilderEntidade.ConverteEntidade<Checklist>(checklist);
+            var registro = await _entityService.BuscarPorId(checklist => checklist.Id == id);
+            
+            if (registro == null) 
+              throw new Exception("O identificador não foi encontrado");
+
+            Builder.Id = id;
+            var checklistBD = await _entityService.Alterar(Builder);
+            return StatusCode(200, checklistBD);
+
+        }
+          catch (System.Exception er)
+          {
+            return StatusCode(401, new { er.Message });
+          }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult> Remover(int id)
+        {
+          try
+          {
+            var entity = await _entityService.BuscarPorId(checklist => checklist.Id == id);
+
+            if (entity == null)
+              throw new Exception("Registro não encontrado");
+
+            await _entityService.Excluir(entity);
+            return StatusCode(200);
+          }
+          catch (System.Exception er)
+          {
+            return StatusCode(401, new { er.Message });
+          }
+        }
+
+  }
 }
