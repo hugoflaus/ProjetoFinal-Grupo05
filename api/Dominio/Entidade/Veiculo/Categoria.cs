934cba245c0b2084a3f419c0c62bee1667ba773e
Lúcio Pimenta <luciosystem1@hotmail.com>
2021-02-20 01:08:10 -0300

    Ajuste na entidade de Categoria, retirado o Required da propriedade URL

--- api/Dominio/Entidade/Veiculo/Categoria.cs
+++ api/Dominio/Entidade/Veiculo/Categoria.cs
@@ -12,9 +12,8 @@ namespace api.Dominio.Entidade.Veiculo
         public int Id { get; set; }
 
         [Required]
-        public string Nome { get; set; }
+        public string Nome { get; set; }          
         
-        [Required]
         public string url { get; set; }
 
         public string Descricao { get; set; }
